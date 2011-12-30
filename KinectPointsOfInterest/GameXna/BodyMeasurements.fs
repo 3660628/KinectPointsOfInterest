f3a781714620b1fe809f93fb239fec2cd9da7943
Ewen Cluley <ec237@sussex.ac.uk>
2011-12-30 16:47:36 +0000

    waist, top of head, bottom of feet working. waist needs tested as only using Phi

---
+++ KinectPointsOfInterest/GameXna/BodyMeasurements.fs
@@ -0,0 +1,179 @@
+﻿namespace BodyData
+
+open Microsoft.Xna.Framework
+open Microsoft.Xna.Framework.Graphics
+
+    type BodyMeasurements(game, hea, shL, shR, shC, hiL, hiR, hiC, foL, foR)=
+        inherit DrawableGameComponent(game)
+        
+        let phi = 1.61803399 //golden ratio
+        
+        //the depth image of the player.  it should only contain one player and each depth should be an int from  850 - 4000 (the valid depths for the depths)  
+        let mutable depthImage:int[] = Array.create 76800 0
+        
+        //joints
+        let mutable head = hea
+        let mutable shoulderL = shL
+        let mutable shoulderR = shR
+        let mutable shoulderC = shC
+        let mutable hipL = hiL
+        let mutable hipR = hiR
+        let mutable hipC = hiC
+        let mutable footL = foL
+        let mutable footR = foR
+        let mutable kneeL = new Vector3(0.0f, 0.0f, 0.0f)
+
+        //points of interest
+        let mutable topOfHead = 0.0f
+        let mutable bottomOfFeet = 0.0f
+        let mutable waist = 0.0f
+        let mutable height = 0.0f
+        let mutable shoulders = 0.0f
+        let mutable hips = 0.0f
+        let mutable knees = 0.0f
+
+        let mutable pointOfInterestLine:Texture2D = null
+        
+        let game = game
+        let mutable spriteBatch = null
+
+        new(game) = new BodyMeasurements(game, new Vector3(0.0f, 0.0f, 0.0f), new Vector3(0.0f, 0.0f, 0.0f), new Vector3(0.0f, 0.0f, 0.0f), new Vector3(0.0f, 0.0f, 0.0f), new Vector3(0.0f, 0.0f, 0.0f), new Vector3(0.0f, 0.0f, 0.0f), new Vector3(0.0f, 0.0f, 0.0f), new Vector3(0.0f, 0.0f, 0.0f), new Vector3(0.0f, 0.0f, 0.0f))
+        
+        member this.completeBody =  //complete body if has joints and depth data
+            if Array.max(depthImage) > 0 then
+                if not(head.Equals(new Vector3(0.0f,0.0f,0.0f))) then
+                    true
+                else
+                    false
+            else
+                false
+
+        
+        //These members find the top and bottom most points of the depth image
+        //The values they return are based on the 2D visualisation space i.e. in the range x=0-240, y=0-320
+        member this.GetTopOfHead =
+            let mutable TOH = Unchecked.defaultof<Vector3>
+            let mutable y = 0
+            while y < 239 && TOH.Equals(Unchecked.defaultof<Vector3>) do
+                let mutable x = 0
+                while x < 319 && TOH.Equals(Unchecked.defaultof<Vector3>) do
+                    let arrayPosition = y * 320 + x  
+                    let depth = depthImage.[arrayPosition]
+                    if depth > 0 then
+                        let coordinates = new Vector3(float32 x, float32 y, float32 depth)
+                        //check it is not a hand raised above the head
+                        let closeEnoughToHead = 
+                            let euclidDist = Vector2.Distance(new Vector2(head.X, head.Y), new Vector2(coordinates.X, coordinates.Y))
+                            if euclidDist < 50.0f then
+                                true
+                            else
+                                false
+                        if closeEnoughToHead then
+                            TOH <- coordinates
+                            System.Diagnostics.Debug.WriteLine("TopOfHead=" + TOH.ToString())
+                    x <- x + 1
+                y <- y + 1
+            topOfHead <- TOH.Y
+            //TOH
+
+        member this.GetBottomOfFeet =
+            let mutable BOF = Unchecked.defaultof<Vector3>
+            let mutable y = 0
+            while y < 239 && BOF.Equals(Unchecked.defaultof<Vector3>) do
+                let mutable x = 0
+                while x < 319 && BOF.Equals(Unchecked.defaultof<Vector3>) do
+                    let arrayPosition = 76799 - (y * 320 + x)  
+                    let depth = depthImage.[arrayPosition]
+                    if depth > 0 then
+                        let coordinates = new Vector3(320.0f - float32 x, 240.0f - float32 y, float32 depth)
+                        let closeEnoughToHead = 
+                            let euclidDist = Vector2.Distance(new Vector2(head.X, head.Y), new Vector2(coordinates.X, coordinates.Y))
+                            if euclidDist < 100.0f then
+                                true
+                            else
+                                false
+                        //if closeEnoughToHead then
+                        BOF <- coordinates
+                        System.Diagnostics.Debug.WriteLine("BottomOfFeet=" + BOF.ToString())
+                    x <- x + 1
+                y <- y + 1
+            bottomOfFeet <- BOF.Y
+            //BOF
+
+        
+
+        //*******************************
+        //Measurement members. Used to find points at which measurements should be taken
+        //*******************************
+
+        //Height measurement
+        member this.MeasureHeightVis=
+            height <- bottomOfFeet - topOfHead
+            //height
+        
+        member this.MeasureHeightWorld=
+            bottomOfFeet * 5.0f - topOfHead * 5.0f 
+        
+        //Ceiling to waist measurement
+        member this.MeasureToWaistVis=
+            let w = -(height / float32 phi) + topOfHead + height
+            waist <- w
+
+        member this.MeasureToWaistWorld=
+            this.MeasureHeightWorld / float32 phi
+
+        //top of screen to shoulders
+        member this.MeasureToShoulders=
+            shoulders <- shoulderC.Y
+            //shoulders
+        
+        member this.MeasureToKnees=
+            knees <- kneeL.Y
+
+        member this.MeasureToHips=
+            hips <- hipC.Y
+
+        member this.SetDepthData dI =
+            depthImage <- dI;
+
+        member this.SetSkeleton (hea, shL, shR, shC, hiL, hiR, hiC, foL, foR, knL, knR) =
+            head <- hea
+            shoulderL <- shL
+            shoulderR <- shR
+            shoulderC <- shC
+            hipL <- hiL
+            hipR <- hiR
+            hipC <- hiC
+            footL <- foL
+            footR <- foR
+            kneeL <- knL
+
+        override this.Initialize()=
+            spriteBatch <- new SpriteBatch(game.GraphicsDevice)
+            pointOfInterestLine <- game.Content.Load<Texture2D>("whiteLine")
+
+        override this.LoadContent()=
+            pointOfInterestLine <- game.Content.Load<Texture2D>("whiteLine")
+
+        override this.Update(gameTime)=
+            if this.completeBody then
+                this.GetTopOfHead
+                this.GetBottomOfFeet
+                this.MeasureHeightVis
+                this.MeasureToWaistVis
+                this.MeasureToShoulders
+                this.MeasureToKnees
+                this.MeasureToHips
+
+
+        override this.Draw(gameTime)=
+            spriteBatch.Begin()
+            //Draw the points of interest lines
+            spriteBatch.Draw(pointOfInterestLine, new Vector2(0.0f, waist * 2.0f), Color.White)//Waist
+            spriteBatch.Draw(pointOfInterestLine, new Vector2(0.0f, topOfHead * 2.0f), Color.White)//Top of Head
+            spriteBatch.Draw(pointOfInterestLine, new Vector2(0.0f, bottomOfFeet * 2.0f), Color.White)//Bottom of feet
+            spriteBatch.Draw(pointOfInterestLine, new Vector2(0.0f, shoulders * 2.0f), Color.White)//Shoulders
+            //spriteBatch.Draw(pointOfInterestLine, new Vector2(0.0f, hips * 2.0f), Color.White)//Hips
+            spriteBatch.Draw(pointOfInterestLine, new Vector2(0.0f, knees * 2.0f), Color.White)//Knees
+            spriteBatch.End()
+        
---
+++ KinectPointsOfInterest/GameXna/Game.fs
@@ -0,0 +1,155 @@
+﻿namespace KinectPointsOfInterest
+
+    module Program=
+        open Microsoft.Xna.Framework
+        open Microsoft.Xna.Framework.Graphics
+
+        open Microsoft.Research.Kinect.Nui
+        open KinectHelperMethods
+
+        open System
+
+        type XnaGame() as this =
+            inherit Game()
+    
+            do this.Content.RootDirectory <- "XnaGameContent"
+            let graphicsDeviceManager = new GraphicsDeviceManager(this)
+
+            let screenWidth, screenHeight = 640, 480
+
+            let mutable sprite : Texture2D = null
+            let mutable spriteBatch : SpriteBatch = null
+
+            let kinect = new Kinect(this)
+    
+            //vertical points of interest. all dimensions are in visualisation space
+            let mutable topOfHeadY = 0.0f
+            let mutable bottomOfFeetY = 0.0f
+            let mutable waistY = 0.0f
+            let mutable shouldersY = 0.0f
+
+            //depth image map
+            let mutable depthImage:Texture2D = null
+
+            override game.Initialize() =
+                graphicsDeviceManager.GraphicsProfile <- GraphicsProfile.HiDef
+                graphicsDeviceManager.PreferredBackBufferWidth <- 640
+                graphicsDeviceManager.PreferredBackBufferHeight <- 480
+                graphicsDeviceManager.ApplyChanges() 
+                spriteBatch <- new SpriteBatch(game.GraphicsDevice)
+                kinect.Initialize()
+                base.Initialize()
+
+            override game.LoadContent() =
+                sprite <- game.Content.Load<Texture2D>("Sprite")
+                base.LoadContent()
+        
+            override game.Update gameTime = 
+                base.Update gameTime
+
+            override game.Draw gameTime = 
+                game.GraphicsDevice.Clear(Color.CornflowerBlue)
+                spriteBatch.Begin()
+
+                //Draw the depth map display
+                if not(depthImage = null) then spriteBatch.Draw(depthImage, new Rectangle(0, 0, screenWidth, screenHeight), Color.White)
+                spriteBatch.End()
+                base.Draw gameTime
+
+            member game.SetDepthImage dI=
+                depthImage <- dI
+
+        and Kinect(game:XnaGame)=
+
+            let nui = Runtime.Kinects.[0]//kinect natural user interface object
+            let game = game
+            let body = new BodyData.BodyMeasurements(game)
+
+            //*****************SKELETON EVENT HANDLER************************************
+            //Manipulates and processes the skeleton data once it has been recieved 
+            //from the sensor
+            //***************************************************************************
+            let SkeletonReady (sender : obj) (args: SkeletonFrameReadyEventArgs)=
+                for skeleton in args.SkeletonFrame.Skeletons do
+                    if skeleton.TrackingState.Equals(SkeletonTrackingState.Tracked) then
+                        let depthWidth, depthHeight = 320, 240
+                        let leftShoulderJ = skeleton.Joints.[JointID.ShoulderLeft]
+                        let leftShoulder = new Vector3(leftShoulderJ.GetScreenPosition(nui, 320, 240).X, leftShoulderJ.GetScreenPosition(nui, 320, 240).Y, leftShoulderJ.Position.Z )
+                        let rightShoulderJ = skeleton.Joints.[JointID.ShoulderRight]
+                        let rightShoulder = new Vector3(rightShoulderJ.GetScreenPosition(nui, 320, 240).X, rightShoulderJ.GetScreenPosition(nui, 320, 240).Y, rightShoulderJ.Position.Z )
+                        let centerShoulderJ = skeleton.Joints.[JointID.ShoulderCenter]
+                        let centerShoulder = new Vector3(centerShoulderJ.GetScreenPosition(nui, 320, 240).X, centerShoulderJ.GetScreenPosition(nui, 320, 240).Y, centerShoulderJ.Position.Z )
+                        let headJ = skeleton.Joints.[JointID.Head]
+                        let head = new Vector3(headJ.GetScreenPosition(nui, 320, 240).X, headJ.GetScreenPosition(nui, 320, 240).Y, headJ.Position.Z )
+                        let leftHipJ = skeleton.Joints.[JointID.HipLeft]
+                        let leftHip = new Vector3(leftHipJ.GetScreenPosition(nui, 320, 240).X, leftHipJ.GetScreenPosition(nui, 320, 240).Y, leftHipJ.Position.Z )
+                        let rightHipJ = skeleton.Joints.[JointID.HipRight]
+                        let rightHip = new Vector3(rightHipJ.GetScreenPosition(nui, 320, 240).X, rightHipJ.GetScreenPosition(nui, 320, 240).Y, rightHipJ.Position.Z )
+                        let centerHipJ = skeleton.Joints.[JointID.HipCenter ]
+                        let centerHip = new Vector3(centerHipJ.GetScreenPosition(nui, 320, 240).X, centerHipJ.GetScreenPosition(nui, 320, 240).Y, centerHipJ.Position.Z )
+                        let leftFootJ = skeleton.Joints.[JointID.FootLeft]
+                        let leftFoot = new Vector3(leftFootJ.GetScreenPosition(nui, 320, 240).X, leftFootJ.GetScreenPosition(nui, 320, 240).Y, leftFootJ.Position.Z )
+                        let rightFootJ = skeleton.Joints.[JointID.FootRight]
+                        let rightFoot = new Vector3(rightFootJ.GetScreenPosition(nui, 320, 240).X, rightFootJ.GetScreenPosition(nui, 320, 240).Y, rightFootJ.Position.Z )
+                        let leftKneeJ = skeleton.Joints.[JointID.KneeLeft]
+                        let leftKnee = new Vector3(leftKneeJ.GetScreenPosition(nui, 320, 240).X, leftKneeJ.GetScreenPosition(nui, 320, 240).Y, leftKneeJ.Position.Z )
+                        let rightKneeJ = skeleton.Joints.[JointID.KneeRight]
+                        let rightKnee = new Vector3(rightKneeJ.GetScreenPosition(nui, 320, 240).X, rightKneeJ.GetScreenPosition(nui, 320, 240).Y, rightKneeJ.Position.Z )
+
+                        body.SetSkeleton(head, leftShoulder, rightShoulder, centerShoulder, leftHip, rightHip, centerHip, leftFoot, rightFoot, leftKnee, rightKnee)
+            //***************************************************************************
+
+            
+            //*****************DEPTHFRAME EVENT HANDLER**********************************
+            //Manipulates and processes the depth data once it has been recieved from 
+            //the sensor
+            //***************************************************************************
+            let DepthReady (sender : obj) (args:ImageFrameReadyEventArgs)=
+                        
+                let maxDist = 4000
+                let minDist = 850
+                let distOffset = maxDist - minDist
+
+                let pImg = args.ImageFrame.Image
+                let img = new Texture2D(game.GraphicsDevice, pImg.Width, pImg.Height)
+                let DepthColor = Array.create (pImg.Width*pImg.Height) (new Color(255,255,255))
+
+                let distancesArray = Array.create (320*240) 0
+
+                for y = 0 to pImg.Height-1 do
+                    for x = 0 to pImg.Width-1 do
+                        let n = (y * pImg.Width + x) * 2
+                        let distance = (int pImg.Bits.[n + 0] >>>3) ||| (int pImg.Bits.[n + 1] <<< 5) //put together bit data as depth
+                        let pI = int (pImg.Bits.[n] &&& 7uy) // gets the player index
+                        
+                        distancesArray.[y*pImg.Width + x] <- if pI > 0 then distance else 0
+
+                        //change distance to colour
+                        let intensity = (if pI > 0 then (255-(255 * Math.Max(int(distance-minDist),0)/distOffset)) else 0) //convert distance into a gray level value between 0 and 255 taking into account min and max distances of the kinect.
+                        let colour = new Color(intensity, intensity, intensity)
+                        DepthColor.[y * pImg.Width + x] <- colour
+
+                img.SetData(DepthColor)
+
+                let copy =  Array.create distancesArray.Length 0 //maybe unnessescary, will check
+                Array.Copy(distancesArray, copy, distancesArray.Length)
+                body.SetDepthData copy
+                game.SetDepthImage img  //pass depth image to the main program  
+            //****************************************************************************   
+            
+            member this.Initialize ()=
+                try 
+                    do nui.Initialize(RuntimeOptions.UseSkeletalTracking ||| RuntimeOptions.UseDepthAndPlayerIndex)
+                    //do nui.SkeletonEngine.TransformSmooth <- true;
+                    do nui.SkeletonFrameReady.AddHandler(new EventHandler<SkeletonFrameReadyEventArgs>(SkeletonReady))
+                    do nui.DepthStream.Open(ImageStreamType.Depth, 2, ImageResolution.Resolution320x240, ImageType.DepthAndPlayerIndex)
+                    do nui.DepthFrameReady.AddHandler(new EventHandler<ImageFrameReadyEventArgs>(DepthReady))
+                    game.Components.Add(body)
+                with
+                    | :? System.InvalidOperationException -> System.Diagnostics.Debug.Write("Kinect not connected!")
+                            
+            member this.Update ()=
+                ()
+
+        let game = new XnaGame()
+        game.Run()
