de2a41a75a648ae25be9b1067be05370a0b59ae0
Ewen Cluley <ec237@sussex.ac.uk>
2012-01-02 18:48:40 +0000

    top of head, bottom of feet, waist work, hips almost using flat method but not for all models

--- KinectPointsOfInterest/GameXna/BodyMeasurements.fs
+++ KinectPointsOfInterest/GameXna/BodyMeasurements.fs
@@ -3,26 +3,22 @@
 open Microsoft.Xna.Framework
 open Microsoft.Xna.Framework.Graphics
 
-    type BodyMeasurements(game, hea, shL, shR, shC, hiL, hiR, hiC, foL, foR)=
+open System
+    
+    //*************************************************************
+    // Methods to measure the body from 3 views, front side and back
+    //
+    // parameters:
+    //      game:GameXna - the XnaGame object that is using this class
+    //      frontBody:Body - the front body view with joints and depth data
+    //      leftSideBody:Body - the side body view with joints and depth data
+    //      backBody:Body - the back body view with joints and depth data
+    //*************************************************************
+    type BodyMeasurements(game, frontBody:Body, leftSideBody:Body, backBody:Body)=
         inherit DrawableGameComponent(game)
         
         let phi = 1.61803399 //golden ratio
         
-        //the depth image of the player.  it should only contain one player and each depth should be an int from  850 - 4000 (the valid depths for the depths)  
-        let mutable depthImage:int[] = Array.create 76800 0
-        
-        //joints
-        let mutable head = hea
-        let mutable shoulderL = shL
-        let mutable shoulderR = shR
-        let mutable shoulderC = shC
-        let mutable hipL = hiL
-        let mutable hipR = hiR
-        let mutable hipC = hiC
-        let mutable footL = foL
-        let mutable footR = foR
-        let mutable kneeL = new Vector3(0.0f, 0.0f, 0.0f)
-
         //points of interest
         let mutable topOfHead = 0.0f
         let mutable bottomOfFeet = 0.0f
@@ -32,26 +28,22 @@ open Microsoft.Xna.Framework.Graphics
         let mutable hips = 0.0f
         let mutable knees = 0.0f
 
+        let mutable pointsFound = false
+
         let mutable pointOfInterestLine:Texture2D = null
+        let mutable frontBodyView:Texture2D = null
+        let mutable sideBodyView:Texture2D = null
         
         let game = game
         let mutable spriteBatch = null
 
-        new(game) = new BodyMeasurements(game, new Vector3(0.0f, 0.0f, 0.0f), new Vector3(0.0f, 0.0f, 0.0f), new Vector3(0.0f, 0.0f, 0.0f), new Vector3(0.0f, 0.0f, 0.0f), new Vector3(0.0f, 0.0f, 0.0f), new Vector3(0.0f, 0.0f, 0.0f), new Vector3(0.0f, 0.0f, 0.0f), new Vector3(0.0f, 0.0f, 0.0f), new Vector3(0.0f, 0.0f, 0.0f))
-        
-        member this.completeBody =  //complete body if has joints and depth data
-            if Array.max(depthImage) > 0 then
-                if not(head.Equals(new Vector3(0.0f,0.0f,0.0f))) then
-                    true
-                else
-                    false
-            else
-                false
+        //new(game) = new BodyMeasurements(game, new Body(new Vector3(0.0f, 0.0f, 0.0f), new Vector3(0.0f, 0.0f, 0.0f), new Vector3(0.0f, 0.0f, 0.0f), new Vector3(0.0f, 0.0f, 0.0f), new Vector3(0.0f, 0.0f, 0.0f), new Vector3(0.0f, 0.0f, 0.0f), new Vector3(0.0f, 0.0f, 0.0f), new Vector3(0.0f, 0.0f, 0.0f), new Vector3(0.0f, 0.0f, 0.0f), new Vector3(0.0f, 0.0f, 0.0f), new Vector3(0.0f, 0.0f, 0.0f)))
 
-        
         //These members find the top and bottom most points of the depth image
         //The values they return are based on the 2D visualisation space i.e. in the range x=0-240, y=0-320
         member this.GetTopOfHead =
+            let depthImage = frontBody.DepthImg
+            let head = frontBody.GetJoint("head")
             let mutable TOH = Unchecked.defaultof<Vector3>
             let mutable y = 0
             while y < 239 && TOH.Equals(Unchecked.defaultof<Vector3>) do
@@ -77,6 +69,7 @@ open Microsoft.Xna.Framework.Graphics
             //TOH
 
         member this.GetBottomOfFeet =
+            let depthImage = frontBody.DepthImg
             let mutable BOF = Unchecked.defaultof<Vector3>
             let mutable y = 0
             while y < 239 && BOF.Equals(Unchecked.defaultof<Vector3>) do
@@ -86,13 +79,6 @@ open Microsoft.Xna.Framework.Graphics
                     let depth = depthImage.[arrayPosition]
                     if depth > 0 then
                         let coordinates = new Vector3(320.0f - float32 x, 240.0f - float32 y, float32 depth)
-                        let closeEnoughToHead = 
-                            let euclidDist = Vector2.Distance(new Vector2(head.X, head.Y), new Vector2(coordinates.X, coordinates.Y))
-                            if euclidDist < 100.0f then
-                                true
-                            else
-                                false
-                        //if closeEnoughToHead then
                         BOF <- coordinates
                         System.Diagnostics.Debug.WriteLine("BottomOfFeet=" + BOF.ToString())
                     x <- x + 1
@@ -100,7 +86,26 @@ open Microsoft.Xna.Framework.Graphics
             bottomOfFeet <- BOF.Y
             //BOF
 
-        
+        member this.GetHips =
+            let kneeL = leftSideBody.GetJoint("leftKnee")
+            let depthImage = leftSideBody.DepthImg
+            let mutable h = 0
+            let mutable hipWidth = 0
+            let mutable y = int (leftSideBody.GetJoint("centerHip").Y) //start at waist as hips are below waist
+            while y < (int kneeL.Y) do //finish at knee as hips are below knee
+                let mutable x = 0
+                let mutable currentFoundWidth = 0
+                while x < 319 do
+                    let arrayPosition = (y * 320 + x)  
+                    let depth = depthImage.[arrayPosition]
+                    if depth > 0 then
+                        currentFoundWidth <- currentFoundWidth + 1
+                    x <- x + 1
+                if currentFoundWidth > hipWidth then
+                    hipWidth <- currentFoundWidth
+                    h <- y
+                y <- y + 1
+            hips <-  float32 h
 
         //*******************************
         //Measurement members. Used to find points at which measurements should be taken
@@ -124,29 +129,14 @@ open Microsoft.Xna.Framework.Graphics
 
         //top of screen to shoulders
         member this.MeasureToShoulders=
-            shoulders <- shoulderC.Y
+            shoulders <- frontBody.GetJoint("centerShoulder").Y
             //shoulders
         
         member this.MeasureToKnees=
-            knees <- kneeL.Y
+            knees <- frontBody.GetJoint("leftKnee").Y
 
         member this.MeasureToHips=
-            hips <- hipC.Y
-
-        member this.SetDepthData dI =
-            depthImage <- dI;
-
-        member this.SetSkeleton (hea, shL, shR, shC, hiL, hiR, hiC, foL, foR, knL, knR) =
-            head <- hea
-            shoulderL <- shL
-            shoulderR <- shR
-            shoulderC <- shC
-            hipL <- hiL
-            hipR <- hiR
-            hipC <- hiC
-            footL <- foL
-            footR <- foR
-            kneeL <- knL
+            hips <- frontBody.GetJoint("centerHip").Y
 
         override this.Initialize()=
             spriteBatch <- new SpriteBatch(game.GraphicsDevice)
@@ -156,24 +146,50 @@ open Microsoft.Xna.Framework.Graphics
             pointOfInterestLine <- game.Content.Load<Texture2D>("whiteLine")
 
         override this.Update(gameTime)=
-            if this.completeBody then
+            if frontBody.CompleteBody && leftSideBody.CompleteBody && not pointsFound then
                 this.GetTopOfHead
                 this.GetBottomOfFeet
                 this.MeasureHeightVis
                 this.MeasureToWaistVis
                 this.MeasureToShoulders
                 this.MeasureToKnees
-                this.MeasureToHips
+                this.GetHips
+                frontBodyView <- this.ConvertDepthToTexture frontBody
+                sideBodyView <- this.ConvertDepthToTexture leftSideBody
+                pointsFound <- true
+
+        member this.ConvertDepthToTexture (b:Body)=
+            let img = new Texture2D(game.GraphicsDevice, 320, 240)
+            let DepthColor = Array.create (320 * 240) (new Color(255,255,255))
+
+            let maxDist = 4000
+            let minDist = 850
+            let distOffset = maxDist - minDist
+
+            for y = 0 to 239 do
+                for x = 0 to 319 do
+                    let n = (y * 320 + x)
+                    let distance = b.DepthImg.[n]
+                    //change distance to colour
+                    let intensity = ((255 * Math.Max(int(distance-minDist),0)/distOffset)) //convert distance into a gray level value between 0 and 255 taking into account min and max distances of the kinect.
+                    let colour = new Color(intensity, intensity, intensity)
+                    DepthColor.[y * 320 + x] <- colour
+            img.SetData(DepthColor)
+            img
 
 
         override this.Draw(gameTime)=
             spriteBatch.Begin()
             //Draw the points of interest lines
-            spriteBatch.Draw(pointOfInterestLine, new Vector2(0.0f, waist * 2.0f), Color.White)//Waist
-            spriteBatch.Draw(pointOfInterestLine, new Vector2(0.0f, topOfHead * 2.0f), Color.White)//Top of Head
-            spriteBatch.Draw(pointOfInterestLine, new Vector2(0.0f, bottomOfFeet * 2.0f), Color.White)//Bottom of feet
-            spriteBatch.Draw(pointOfInterestLine, new Vector2(0.0f, shoulders * 2.0f), Color.White)//Shoulders
-            //spriteBatch.Draw(pointOfInterestLine, new Vector2(0.0f, hips * 2.0f), Color.White)//Hips
-            spriteBatch.Draw(pointOfInterestLine, new Vector2(0.0f, knees * 2.0f), Color.White)//Knees
+            if frontBodyView <> null then
+                spriteBatch.Draw(frontBodyView, new Vector2(320.0f, 0.0f), Color.White)//Waist
+            if sideBodyView <> null then
+                spriteBatch.Draw(sideBodyView, new Vector2(640.0f, 0.0f), Color.White)//Waist
+            spriteBatch.Draw(pointOfInterestLine, new Vector2(320.0f, waist), Color.White)//Waist
+            spriteBatch.Draw(pointOfInterestLine, new Vector2(320.0f, topOfHead), Color.White)//Top of Head
+            spriteBatch.Draw(pointOfInterestLine, new Vector2(320.0f, bottomOfFeet), Color.White)//Bottom of feet
+            spriteBatch.Draw(pointOfInterestLine, new Vector2(320.0f, shoulders), Color.White)//Shoulders
+            spriteBatch.Draw(pointOfInterestLine, new Vector2(320.0f, hips), Color.White)//Hips
+            spriteBatch.Draw(pointOfInterestLine, new Vector2(320.0f, knees), Color.White)//Knees
             spriteBatch.End()
         
--- KinectPointsOfInterest/GameXna/Game.fs
+++ KinectPointsOfInterest/GameXna/Game.fs
@@ -3,11 +3,14 @@
     module Program=
         open Microsoft.Xna.Framework
         open Microsoft.Xna.Framework.Graphics
+        open Microsoft.Xna.Framework.Audio
 
         open Microsoft.Research.Kinect.Nui
         open KinectHelperMethods
 
         open System
+
+        open BodyData
 
         type XnaGame() as this =
             inherit Game()
@@ -15,13 +18,17 @@
             do this.Content.RootDirectory <- "XnaGameContent"
             let graphicsDeviceManager = new GraphicsDeviceManager(this)
 
-            let screenWidth, screenHeight = 640, 480
+            let screenWidth, screenHeight = 960, 480
 
             let mutable sprite : Texture2D = null
             let mutable spriteBatch : SpriteBatch = null
 
             let kinect = new Kinect(this)
     
+            let mutable timer = 10000.0f
+            let mutable finished = false
+            let mutable frontBody, backBody, sideBody = null, null, null
+
             //vertical points of interest. all dimensions are in visualisation space
             let mutable topOfHeadY = 0.0f
             let mutable bottomOfFeetY = 0.0f
@@ -30,11 +37,12 @@
 
             //depth image map
             let mutable depthImage:Texture2D = null
+            let mutable clickSound:SoundEffect = null
 
             override game.Initialize() =
                 graphicsDeviceManager.GraphicsProfile <- GraphicsProfile.HiDef
-                graphicsDeviceManager.PreferredBackBufferWidth <- 640
-                graphicsDeviceManager.PreferredBackBufferHeight <- 480
+                graphicsDeviceManager.PreferredBackBufferWidth <- screenWidth
+                graphicsDeviceManager.PreferredBackBufferHeight <- screenHeight
                 graphicsDeviceManager.ApplyChanges() 
                 spriteBatch <- new SpriteBatch(game.GraphicsDevice)
                 kinect.Initialize()
@@ -42,9 +50,26 @@
 
             override game.LoadContent() =
                 sprite <- game.Content.Load<Texture2D>("Sprite")
+                clickSound <- game.Content.Load<SoundEffect>("click_1")
                 base.LoadContent()
         
             override game.Update gameTime = 
+
+                timer <- timer - float32 gameTime.ElapsedGameTime.TotalMilliseconds
+
+                if timer <= -10000.0f && backBody = null then
+                    clickSound.Play() |> ignore
+                    backBody <- kinect.CaptureBody
+                else if timer <= -5000.0f && sideBody = null then
+                    clickSound.Play() |> ignore
+                    sideBody <- kinect.CaptureBody
+                else if timer <= 0.0f && frontBody = null then
+                    clickSound.Play() |> ignore
+                    frontBody <- kinect.CaptureBody
+                    
+                if frontBody <> null && backBody <> null && sideBody <> null && not finished then
+                    game.Components.Add(new BodyMeasurements(this, frontBody, sideBody, backBody))
+                    finished <- true
                 base.Update gameTime
 
             override game.Draw gameTime = 
@@ -52,7 +77,7 @@
                 spriteBatch.Begin()
 
                 //Draw the depth map display
-                if not(depthImage = null) then spriteBatch.Draw(depthImage, new Rectangle(0, 0, screenWidth, screenHeight), Color.White)
+                if not(depthImage = null) then spriteBatch.Draw(depthImage, new Rectangle(0, 0, screenWidth/3, screenHeight/2), Color.White)
                 spriteBatch.End()
                 base.Draw gameTime
 
@@ -63,14 +88,65 @@
 
             let nui = Runtime.Kinects.[0]//kinect natural user interface object
             let game = game
-            let body = new BodyData.BodyMeasurements(game)
+            let body = new BodyData.Body()
+
+            let mutable lastSkeletonArgs = null
+            let mutable lastDepthArgs = null
+
+            let maxDist = 4000
+            let minDist = 850
+            let distOffset = maxDist - minDist
 
             //*****************SKELETON EVENT HANDLER************************************
             //Manipulates and processes the skeleton data once it has been recieved 
             //from the sensor
             //***************************************************************************
             let SkeletonReady (sender : obj) (args: SkeletonFrameReadyEventArgs)=
-                for skeleton in args.SkeletonFrame.Skeletons do
+                lastSkeletonArgs <- args
+            //***************************************************************************
+
+            
+            //*****************DEPTHFRAME EVENT HANDLER**********************************
+            //Manipulates and processes the depth data once it has been recieved from 
+            //the sensor
+            //***************************************************************************
+            let DepthReady (sender : obj) (args:ImageFrameReadyEventArgs)=      
+                lastDepthArgs <- args
+                
+                let pImg = args.ImageFrame.Image
+                let img = new Texture2D(game.GraphicsDevice, pImg.Width, pImg.Height)
+                let DepthColor = Array.create (pImg.Width*pImg.Height) (new Color(255,255,255))
+
+                let distancesArray = Array.create (320*240) 0
+
+                for y = 0 to pImg.Height-1 do
+                    for x = 0 to pImg.Width-1 do
+                        let n = (y * pImg.Width + x) * 2
+                        let distance = (int pImg.Bits.[n + 0] >>>3) ||| (int pImg.Bits.[n + 1] <<< 5) //put together bit data as depth
+                        let pI = int (pImg.Bits.[n] &&& 7uy) // gets the player index
+                        //change distance to colour
+                        let intensity = (if pI > 0 then (255-(255 * Math.Max(int(distance-minDist),0)/distOffset)) else 0) //convert distance into a gray level value between 0 and 255 taking into account min and max distances of the kinect.
+                        let colour = new Color(intensity, intensity, intensity)
+                        DepthColor.[y * pImg.Width + x] <- colour
+                img.SetData(DepthColor)
+                game.SetDepthImage img
+
+            //****************************************************************************   
+            
+            member this.Initialize ()=
+                try 
+                    do nui.Initialize(RuntimeOptions.UseSkeletalTracking ||| RuntimeOptions.UseDepthAndPlayerIndex)
+                    //do nui.SkeletonEngine.TransformSmooth <- true;
+                    do nui.SkeletonFrameReady.AddHandler(new EventHandler<SkeletonFrameReadyEventArgs>(SkeletonReady))
+                    do nui.DepthStream.Open(ImageStreamType.Depth, 2, ImageResolution.Resolution320x240, ImageType.DepthAndPlayerIndex)
+                    do nui.DepthFrameReady.AddHandler(new EventHandler<ImageFrameReadyEventArgs>(DepthReady))
+                    
+                with
+                    | :? System.InvalidOperationException -> System.Diagnostics.Debug.Write("Kinect not connected!")
+                            
+            member this.CaptureBody =
+                let body = new BodyData.Body()
+                for skeleton in lastSkeletonArgs.SkeletonFrame.Skeletons do
                     if skeleton.TrackingState.Equals(SkeletonTrackingState.Tracked) then
                         let depthWidth, depthHeight = 320, 240
                         let leftShoulderJ = skeleton.Joints.[JointID.ShoulderLeft]
@@ -95,24 +171,10 @@
                         let leftKnee = new Vector3(leftKneeJ.GetScreenPosition(nui, 320, 240).X, leftKneeJ.GetScreenPosition(nui, 320, 240).Y, leftKneeJ.Position.Z )
                         let rightKneeJ = skeleton.Joints.[JointID.KneeRight]
                         let rightKnee = new Vector3(rightKneeJ.GetScreenPosition(nui, 320, 240).X, rightKneeJ.GetScreenPosition(nui, 320, 240).Y, rightKneeJ.Position.Z )
-
-                        body.SetSkeleton(head, leftShoulder, rightShoulder, centerShoulder, leftHip, rightHip, centerHip, leftFoot, rightFoot, leftKnee, rightKnee)
-            //***************************************************************************
-
-            
-            //*****************DEPTHFRAME EVENT HANDLER**********************************
-            //Manipulates and processes the depth data once it has been recieved from 
-            //the sensor
-            //***************************************************************************
-            let DepthReady (sender : obj) (args:ImageFrameReadyEventArgs)=
                         
-                let maxDist = 4000
-                let minDist = 850
-                let distOffset = maxDist - minDist
+                        body.SetSkeleton(head, leftShoulder, rightShoulder, centerShoulder, leftHip, rightHip, centerHip, leftFoot,rightFoot, leftKnee, rightKnee)
 
-                let pImg = args.ImageFrame.Image
-                let img = new Texture2D(game.GraphicsDevice, pImg.Width, pImg.Height)
-                let DepthColor = Array.create (pImg.Width*pImg.Height) (new Color(255,255,255))
+                let pImg = lastDepthArgs.ImageFrame.Image
 
                 let distancesArray = Array.create (320*240) 0
 
@@ -124,32 +186,8 @@
                         
                         distancesArray.[y*pImg.Width + x] <- if pI > 0 then distance else 0
 
-                        //change distance to colour
-                        let intensity = (if pI > 0 then (255-(255 * Math.Max(int(distance-minDist),0)/distOffset)) else 0) //convert distance into a gray level value between 0 and 255 taking into account min and max distances of the kinect.
-                        let colour = new Color(intensity, intensity, intensity)
-                        DepthColor.[y * pImg.Width + x] <- colour
-
-                img.SetData(DepthColor)
-
-                let copy =  Array.create distancesArray.Length 0 //maybe unnessescary, will check
-                Array.Copy(distancesArray, copy, distancesArray.Length)
-                body.SetDepthData copy
-                game.SetDepthImage img  //pass depth image to the main program  
-            //****************************************************************************   
-            
-            member this.Initialize ()=
-                try 
-                    do nui.Initialize(RuntimeOptions.UseSkeletalTracking ||| RuntimeOptions.UseDepthAndPlayerIndex)
-                    //do nui.SkeletonEngine.TransformSmooth <- true;
-                    do nui.SkeletonFrameReady.AddHandler(new EventHandler<SkeletonFrameReadyEventArgs>(SkeletonReady))
-                    do nui.DepthStream.Open(ImageStreamType.Depth, 2, ImageResolution.Resolution320x240, ImageType.DepthAndPlayerIndex)
-                    do nui.DepthFrameReady.AddHandler(new EventHandler<ImageFrameReadyEventArgs>(DepthReady))
-                    game.Components.Add(body)
-                with
-                    | :? System.InvalidOperationException -> System.Diagnostics.Debug.Write("Kinect not connected!")
-                            
-            member this.Update ()=
-                ()
+                body.DepthImg <- distancesArray
+                body
 
         let game = new XnaGame()
         game.Run()
